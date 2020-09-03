6f46fe2b7ff3c4bae6f9c5eccde65713a0305fad
SaraFogo <sarafogo.ss@gmail.com>
2020-09-03 19:17:02 -0300

    build agora e docs

---
+++ escola-mais/docs/precache-manifest.f1d5ab1b55d1b9748eb97400bd0cd598.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35481dbb73175ae584f14043d25a62d0",
+    "url": "/escola-mais/index.html"
+  },
+  {
+    "revision": "85c2dd6aa08a46a96dc5",
+    "url": "/escola-mais/static/css/main.04572241.chunk.css"
+  },
+  {
+    "revision": "882239a36edecb8f6c76",
+    "url": "/escola-mais/static/js/2.3c06d22f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/escola-mais/static/js/2.3c06d22f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c2dd6aa08a46a96dc5",
+    "url": "/escola-mais/static/js/main.2338b837.chunk.js"
+  },
+  {
+    "revision": "dc916d0d3223b1bb06f5",
+    "url": "/escola-mais/static/js/runtime-main.9446ea5c.js"
+  },
+  {
+    "revision": "32b30918182f40e235f61f494333967b",
+    "url": "/escola-mais/static/media/321660.32b30918.jpg"
+  }
+]);
